5495e43a57c4919894617e32296ee009abeda4ca
Michail Tsyganov <tsyganov.michail@yandex.ru>
2016-02-14 19:13:34 +0300

    Проверка почти работает.

--- main.c
+++ main.c
@@ -12,7 +12,6 @@
 #include <stdio.h>
 #include <signal.h>
 #include <setjmp.h>
-#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
@@ -20,54 +19,72 @@
 
 sigjmp_buf buffer; // область памяти для запоминания состояния процесса
 
-char file_buff[10000];
-char file_name[]={"work.txt"};
-char copy_buff[1000];
-int capacity;
-FILE *in, *out;
+// Файл, который копируем
+int in;                                 // Дескриптор
+static int file_cap=0;                  // Ёмкость файла
+char file_buff[10000];                  // Буфер под файл
+char file_name[1000]="work.txt";        // Имя файла
+
+// Файл - копия
+int out;                    // Дескриптор
+static int copy_cap=0;      // Ёмкость копии
+char copy_buff[1000];       // Буфер под копию
+char *new_name;             // Новое имя копии
+
+char rand_buf[1000];
 int now=0;
-int cap=0;
 int ii=0;
 
 // Функция обработки прерывания
-void HandleInterruption();
-
-// Функция проверки успешного открытия файла
-void CheckFile(FILE *file)
+void HandleInterruption()
 {
-    if (!file)
+    printf("Прерывание!\n");
+
+    ii++;
+    if (ii==1)
     {
-        printf("Ошибка открытия файла!\n");
-        exit(1);
+        printf("Переименование копии в исходный файл:\n\n");
+        char copy[]="copy/";
+        new_name=strcat(copy, file_name);
+        printf("%s\n", new_name);
+        rename("copy/copy", new_name);
+        int i;
+        printf("В файле записано:\n");
+        for(i=0; i<now; ++i)
+        {
+            printf("%c", copy_buff[i]);
+        }
+        printf("\nКонец записи!\n");
     }
-    printf("Файл открыт!\n");
+    //
+    siglongjmp(buffer, 1);
 }
 
 int main()
 {
     int fd[2], i;
 
+    // Мой sigaction
     struct sigaction cpnew;
     cpnew.sa_handler=HandleInterruption;
     cpnew.sa_flags=0;
-
-    printf("Введите имя файла для копирования: ");
-    //scanf("%s", file_name);
-
-    printf("Открываем файл %s\n", file_name);
-
-    in=fopen(file_name, "r");
-    CheckFile(in);
-
-    while (fscanf(in, "%c", &file_buff[cap])==1)
-    {
-        cap++;
-    }
-
     sigprocmask(0, 0, &cpnew.sa_mask);
     sigaction(SIGINT, &cpnew, 0);
-    //sigsetjmp(buffer, 1);
+    sigsetjmp(buffer, 1);
 
+    //write(1, "Введите имя файла для копирования: ", 35)
+    printf("Введите имя файла для копирования: ");
+    //scanf("%s", file_name);
+    printf("Открываем файл %s\n", file_name);
+
+    fd[1]=open(file_name, 0);
+
+    while (read(fd[1], &file_buff[file_cap], 1)!=0)
+    {
+        //printf("%c", file_buff[cap]);
+        file_cap++;
+    }
+    sigsetjmp(buffer, 1);
     if (pipe(fd) == -1)
     {
         printf("Ошибка создания межпроцессного канала!");
@@ -84,44 +101,51 @@ int main()
             printf("Процесс-сын\n");
             close(fd[0]);
             dup2(fd[1],1); // Дублирование на фд
-            for(i=0; i<cap; ++i)
+            for(i=0; i<file_cap; ++i)
             {
                 printf("%c", file_buff[i]);
             }
 
         default:
             wait(NULL);
-            //sigsetjmp(buffer, 1);
             printf("Процесс-родитель\n");
-            capacity=read(fd[0], copy_buff, cap);
-            out=fopen("copy/copy", "w");
-            CheckFile(out);
-            printf("Начинаю копирование. Можно прерывать.");
-            //sigsetjmp(buffer, 1);
-            sleep(1);
-            for(i=0; i<capacity; ++i)
+            copy_cap=read(fd[0], copy_buff, file_cap);
+            fd[0]=creat("copy/copy", 0700);
+            printf("Начинаю копирование. Можно прерывать.\n\n");
+            for(i=0; i<copy_cap; ++i)
             {
                 now++;
-                fprintf(out, "%c", copy_buff[i]);
+                //fprintf(out, "%c", copy_buff[i]);
+                write(fd[0], &copy_buff[i], 1);
                 printf("%c", copy_buff[i]);
                 if (copy_buff[i]=='\n')
                 {
-                    sleep(1);
+                    // Контрольная точка, где сохраняется "слепок программы"
+                    // Все переменные, 1 - сохраняется сигнальная маска
+                    // Возвращается на эту же точку после прерывания
                     sigsetjmp(buffer, 1);
+                    sleep(1);
                 }
-
             }
-            sleep(1);
             int status;
+            printf("%d\n", status);
+            //system("/usr/bin/cmp -s work.txt test.txt");
+
             if (ii >= 1)
-               execl("/bin/cmp", "cmp","-s", file_name, "copy-1", "> ", status, NULL);
+                execl("/usr/bin/cmp", "cmp","-s", file_name, new_name, '>', &status, NULL);
             else
-                execl("/bin/cmp", "cmp","-s", file_name, "copy", ">", status, NULL);
+                execl("/usr/bin/cmp", "cmp","-s", file_name, "copy/copy", '>', &status, NULL);
+            //read(0, &status, 1);
+            //scanf("%c", &status);
+            //dup2(1, status);
+            //system("/bin/echo $?");
+
+            printf("status %d\n", status);
             switch (status)
             {
                 case 0:
                     printf("Файлы одинаковые, удаляю исходный.\n");
-                    remove(file_name);
+                    //remove(file_name);
                     break;
                 case 1:
                     printf("Файлы различны!\n");
@@ -129,34 +153,10 @@ int main()
                 case 2:
                     printf("Ошибка!\n");
                     break;
-                case -1:
-                    printf("Ошибка execl!\n");
-                    break;
             }
+
     }
-    printf("end.\n");
+    printf("End.\n");
     return 0;
 }
 
-void HandleInterruption()
-{
-    printf("Прерывание!\n");
-
-    ii++;
-    if (ii==1)
-    {
-        printf("Переименование копии в исходный файл:\n\n");
-        char copy[]="copy/";
-        char *new_name=strcat(copy, file_name);
-        printf("%s\n", new_name);
-        rename("copy/copy", new_name);
-        int i;
-        for(i=0; i<now; ++i)
-        {
-            printf("%c", copy_buff[i]);
-        }
-        printf("\nКонец записи!\n");
-
-    }
-    siglongjmp(buffer, 1);
-}
